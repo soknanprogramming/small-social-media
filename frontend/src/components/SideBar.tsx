60906f552fea39ec630dc72493115ddcf9b2673d
soknanprogramming <poch.soknan@gmail.com>
2026-03-27 06:41:17 +0700

    add logout on frontent

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -4,10 +4,13 @@ import { useAppSelector, useAppDispatch } from "../app/hooks";
 // import { deleteToken } from "../features/auth/jwtSlice";
 import type { RootState } from "../app/store";
 import { setLoggedIn } from "../features/auth/loginSlice";
+import api from "../libs/axios";
 
 const SideBar: React.FC = () => {
   // const token = useAppSelector((state: RootState) => state.jwt.token);
-  const isLoggedIn = useAppSelector((state: RootState) => state.login.isLoggedIn);
+  const isLoggedIn = useAppSelector(
+    (state: RootState) => state.login.isLoggedIn,
+  );
   const dispatch = useAppDispatch();
 
   console.log("Sidebar - isLoggedIn:", isLoggedIn);
@@ -16,34 +19,45 @@ const SideBar: React.FC = () => {
     <div className="w-30 bg-amber-200 h-screen sticky left-0 top-0 mr-2 p-2">
       <nav>
         <ul>
-            {
-                isLoggedIn && (
-                    <li className="hover:text-blue-600">
-                        <Link to="/profile">Profile</Link>
-                    </li>
-                )
-            }
+          {isLoggedIn && (
+            <li className="hover:text-blue-600">
+              <Link to="/profile">Profile</Link>
+            </li>
+          )}
           <li className="hover:text-blue-600">
             <Link to="/">Home</Link>
           </li>
           {/* <li className="hover:text-blue-600">
             <Link to="/about">About</Link>
           </li> */}
-            {!isLoggedIn ? (
-              <>
-                <li className="hover:text-blue-600">
-                  <Link to="/register">Register</Link>
-                </li>
-                <li className="hover:text-blue-600">
-                  <Link to="/login">Login</Link>
-                </li>
-              </>
-            ) : (
-                <li className="hover:text-blue-600">
-                    <button onClick={() => dispatch(setLoggedIn(false))}>Logout</button>
-                </li>
-            )}
-
+          {!isLoggedIn ? (
+            <>
+              <li className="hover:text-blue-600">
+                <Link to="/register">Register</Link>
+              </li>
+              <li className="hover:text-blue-600">
+                <Link to="/login">Login</Link>
+              </li>
+            </>
+          ) : (
+            <li className="hover:text-blue-600">
+              <button
+                onClick={() => {
+                  api
+                    .post("/api/users/logout")
+                    .then(() => {
+                      dispatch(setLoggedIn(false));
+                      console.log("Logout successful");
+                    })
+                    .catch((err) => {
+                      console.error("Logout failed:", err);
+                    });
+                }}
+              >
+                Logout
+              </button>
+            </li>
+          )}
         </ul>
       </nav>
     </div>
