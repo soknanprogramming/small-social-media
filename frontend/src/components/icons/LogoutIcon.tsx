d243b18c1662f35f8e679ab55c26c119b6abde62
soknanprogramming <poch.soknan@gmail.com>
2026-03-29 20:03:51 +0700

    add more reusable component for icons

---
+++ frontend/src/components/icons/LogoutIcon.tsx
@@ -0,0 +1,16 @@
+export const LogoutIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="w-4 h-4"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={1.8}
+    viewBox="0 0 24 24"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
+    />
+  </svg>
+)
