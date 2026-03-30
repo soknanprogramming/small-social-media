dc838a9c269d0e0901573800539551bc2f918940
soknanprogramming <poch.soknan@gmail.com>
2026-03-30 19:20:58 +0700

    update router style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import SideBar from "./components/SideBar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./features/page/Home";
 import About from "./features/page/About";
 import Register from "./features/auth/Register";
@@ -7,10 +7,19 @@ import Login from "./features/auth/Login";
 import Profile from "./features/profile/Profile";
 import PostPage from "./features/posts/PostPage";
 import MyPostPage from "./features/posts/MyPostPage";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const handler = () => navigate("/login");
+
+    window.addEventListener("unauthorized", handler);
+    return () => window.removeEventListener("unauthorized", handler);
+  }, [navigate]);
+
   return (
-    <BrowserRouter>
       <div className="flex">
         <SideBar />
         <div className="flex-1">
@@ -25,7 +34,6 @@ function App() {
           </Routes>
         </div>
       </div>
-    </BrowserRouter>
   );
 }
 
